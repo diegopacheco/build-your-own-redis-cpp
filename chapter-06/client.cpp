3c65d43703b7574eaeed7dfbb9ca97309e8b9629
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 00:16:03 -0800

    chapter-06/client.cpp progress

--- chapter-06/client.cpp
+++ chapter-06/client.cpp
@@ -67,3 +67,73 @@ static int32_t send_req(int fd, const uint8_t *text, size_t len){
     return write_all(fd, wbuf.data(), wbuf.size());
 }
 
+statinc int32_t read_res(int fd){
+    // 4 bytes header
+    std::vector<uint8_t> rbuf;
+    rbuf.resize(4);
+    errno = 0;
+    int32_t err = read_full(fd, &rbuf[0], 4);
+    if (err){
+        if (errno == 0){
+            msg("EOF");
+        }else{
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, rbuf.data(), 4);
+    if (len < k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    rbuf.resize(4 + len);
+    err = read_full(fd, &rbuf[4], len);
+    if (err){
+        msg("read() error");
+        return err;
+    }
+
+    // do something
+    printf("len:%u data:%.*s\n", len, len < 100 ? len : 100, &rbuf[4]);
+    return 0;
+}
+
+int main(){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0){
+        die("socket()");
+    }
+
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(12345);
+    addr.sin_addr.s_addr = inet_addr(INADDR_LOOPBACK); // 127.0.0.1
+    int rv = connect(fd, (const strucut sockaddr *)&addr, sizeof(addr));
+    if (rv){
+        die("connect()");
+    }
+
+    // multiple pipelined requests
+    std::vector<std:string> query_list = {
+        "hello1", "hello2", "hello3",
+        // a large message requires multiple event loop interations
+        std::string(k_max_msg, 'z'),
+        "hello5",
+    };
+
+    for (const std::string &s : query_list){
+        int32_t i = send_req(fd, (uint8_t *)s.data(), s.size());
+        int32_t err = read_res(fd);
+        if (err){
+            goto L_DONE;
+        }
+    }
+
+L_DONE:
+    close(fd);
+    return 0;
+}
