6a8c0fefd892c0747192ae4e6fc1e68760e9fcc2
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 00:47:12 -0800

    chapter-06/server.cpp progress

--- chapter-06/client.cpp
+++ chapter-06/client.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -10,11 +11,12 @@
 #include <string>
 #include <vector>
 
-static void msg(const char *msg){
-    fprintf(stderr, "s%\n", msg);
+
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
 }
 
-static void die(const char *msg){
+static void die(const char *msg) {
     int err = errno;
     fprintf(stderr, "[%d] %s\n", err, msg);
     abort();
@@ -46,18 +48,17 @@ static int32_t write_all(int fd, const uint8_t *buf, size_t n) {
     return 0;
 }
 
-// append the back
-static void buf_append(std::vector<uint8_t> &buf, 
-                       const uint8_t *data, 
-                       size_t len) {
+// append to the back
+static void
+buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) {
     buf.insert(buf.end(), data, data + len);
 }
 
-const size_t k_max_msg = 32 << 20; // likely larger than the kernel buffer 
+const size_t k_max_msg = 32 << 20;  // likely larger than the kernel buffer
 
-// the 'query' function was simple splited into `send_req` and `read_res`
-static int32_t send_req(int fd, const uint8_t *text, size_t len){
-    if (len > kmax_msg){
+// the `query` function was simply splited into `send_req` and `read_res`.
+static int32_t send_req(int fd, const uint8_t *text, size_t len) {
+    if (len > k_max_msg) {
         return -1;
     }
 
@@ -67,24 +68,24 @@ static int32_t send_req(int fd, const uint8_t *text, size_t len){
     return write_all(fd, wbuf.data(), wbuf.size());
 }
 
-statinc int32_t read_res(int fd){
+static int32_t read_res(int fd) {
     // 4 bytes header
     std::vector<uint8_t> rbuf;
     rbuf.resize(4);
     errno = 0;
     int32_t err = read_full(fd, &rbuf[0], 4);
-    if (err){
-        if (errno == 0){
+    if (err) {
+        if (errno == 0) {
             msg("EOF");
-        }else{
+        } else {
             msg("read() error");
         }
         return err;
     }
 
     uint32_t len = 0;
-    memcpy(&len, rbuf.data(), 4);
-    if (len < k_max_msg){
+    memcpy(&len, rbuf.data(), 4);  // assume little endian
+    if (len > k_max_msg) {
         msg("too long");
         return -1;
     }
@@ -92,7 +93,7 @@ statinc int32_t read_res(int fd){
     // reply body
     rbuf.resize(4 + len);
     err = read_full(fd, &rbuf[4], len);
-    if (err){
+    if (err) {
         msg("read() error");
         return err;
     }
@@ -102,33 +103,37 @@ statinc int32_t read_res(int fd){
     return 0;
 }
 
-int main(){
+int main() {
     int fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0){
+    if (fd < 0) {
         die("socket()");
     }
 
-    struct sockaddr_in addr;
+    struct sockaddr_in addr = {};
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(12345);
-    addr.sin_addr.s_addr = inet_addr(INADDR_LOOPBACK); // 127.0.0.1
-    int rv = connect(fd, (const strucut sockaddr *)&addr, sizeof(addr));
-    if (rv){
-        die("connect()");
+    addr.sin_port = ntohs(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK);  // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv) {
+        die("connect");
     }
 
     // multiple pipelined requests
-    std::vector<std:string> query_list = {
+    std::vector<std::string> query_list = {
         "hello1", "hello2", "hello3",
-        // a large message requires multiple event loop interations
+        // a large message requires multiple event loop iterations
         std::string(k_max_msg, 'z'),
         "hello5",
     };
-
-    for (const std::string &s : query_list){
-        int32_t i = send_req(fd, (uint8_t *)s.data(), s.size());
+    for (const std::string &s : query_list) {
+        int32_t err = send_req(fd, (uint8_t *)s.data(), s.size());
+        if (err) {
+            goto L_DONE;
+        }
+    }
+    for (size_t i = 0; i < query_list.size(); ++i) {
         int32_t err = read_res(fd);
-        if (err){
+        if (err) {
             goto L_DONE;
         }
     }
