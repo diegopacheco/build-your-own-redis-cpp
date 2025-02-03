5e7910dd0f95e0885d24a9f2976522e83c3811bf
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 00:05:06 -0800

    chapter-06/client.cpp progress

--- chapter-06/client.cpp
+++ chapter-06/client.cpp
@@ -9,3 +9,61 @@
 #include <netinet/ip.h>
 #include <string>
 #include <vector>
+
+static void msg(const char *msg){
+    fprintf(stderr, "s%\n", msg);
+}
+
+static void die(const char *msg){
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static int32_t read_full(int fd, uint8_t *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = read(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error, or unexpected EOF
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+static int32_t write_all(int fd, const uint8_t *buf, size_t n) {
+    while (n > 0) {
+        ssize_t rv = write(fd, buf, n);
+        if (rv <= 0) {
+            return -1;  // error
+        }
+        assert((size_t)rv <= n);
+        n -= (size_t)rv;
+        buf += rv;
+    }
+    return 0;
+}
+
+// append the back
+static void buf_append(std::vector<uint8_t> &buf, 
+                       const uint8_t *data, 
+                       size_t len) {
+    buf.insert(buf.end(), data, data + len);
+}
+
+const size_t k_max_msg = 32 << 20; // likely larger than the kernel buffer 
+
+// the 'query' function was simple splited into `send_req` and `read_res`
+static int32_t send_req(int fd, const uint8_t *text, size_t len){
+    if (len > kmax_msg){
+        return -1;
+    }
+
+    std::vector<uint8_t> wbuf;
+    buf_append(wbuf, (const uint8_t *)&len, 4);
+    buf_append(wbuf, text, len);
+    return write_all(fd, wbuf.data(), wbuf.size());
+}
+
