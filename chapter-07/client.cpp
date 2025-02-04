3928e58f550be1246a0cbdc7dd7659b0d6e6ea6d
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 22:19:16 -0800

    chapter-07/client.cpp progress

--- chapter-07/client.cpp
+++ chapter-07/client.cpp
@@ -71,3 +71,43 @@ static int32_t send_req(int fd, const std::vector<std::string> &cmd){
     }
     return write_all(fd, wbuf, 4 + len);
 }
+
+static int32_t read_res(int fd){
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
+    errno = 0;
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err){
+        if (errno == 0){
+            msg("EOF");
+        }else{
+            msg("read() error");
+        }
+        return err;
+    }
+
+    uint32_t len = 0;
+    memcpy(&len, &rbuf[0], 4); // assume little endian
+    if (len > k_max_msg){
+        msg("too long");
+        return -1;
+    }
+
+    // reply body
+    err = read_full(fd, &rbuf[4], len);
+    if (err){
+        msg("read() error");
+        return err;
+    }
+
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4){
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len -4, &rbuf[8]);
+    return 0;
+}
+
