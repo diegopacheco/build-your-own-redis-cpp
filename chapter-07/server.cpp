8834c9292901e2f4599f6f44847c577cbcc0f564
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 22:28:03 -0800

    chapter-07/server.cpp progress

--- chapter-07/server.cpp
+++ chapter-07/server.cpp
@@ -17,3 +17,33 @@
 #include <vector>
 #include <map>
 
+static void msg(const char *msg){
+    fprintf(stderr, "%s\n", msg);
+}
+
+static void msg_errno(const char *msg){
+    fprintf(stderr, "[%d] %s\n", errno, msg);
+}
+
+static void die(const char *msg){
+    fprintf(stderr, "[%d] %s\n", msg);
+    abort();
+}
+
+static void fd_set_nb(int fd){
+    errno = 0;
+    int flags = fcntl(fd, F_GETFL, 0);
+    if (errno){
+        die("fcntl(F_GETFL) error");
+        return;
+    }
+
+    flags |= O_NONBLOCK;
+
+    errno = 0;
+    (void)fcntl(fd, F_SETFL, flags);
+    if (errno){
+        die("fcntl(F_SETFL) error");
+    }
+}
+
