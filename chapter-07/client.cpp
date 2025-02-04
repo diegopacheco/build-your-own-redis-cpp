cffb88f24d3acc32aa485855bc6460eb40c2c975
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 22:01:19 -0800

    chapter-07/ progress + proper cpp config for vscode

---
+++ chapter-07/client.cpp
@@ -0,0 +1,17 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <string>
+#include <vector>
+
+static void msg(const char *msg){
+    fprintf(stderr, "%s\n", msg);
+}
+
