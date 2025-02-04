604655e35bd0ed48a8ba21b4257a0144d87adaf7
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-03 22:23:41 -0800

    chapter-07/client.cpp progress

--- chapter-07/client.cpp
+++ chapter-07/client.cpp
@@ -111,3 +111,35 @@ static int32_t read_res(int fd){
     return 0;
 }
 
+int main(int argc, char **argv){
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0){
+        die("socket() failed");
+    }   
+
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(1234);
+    addr.sin_addr.s_addr = ntohl(INADDR_LOOPBACK); // 127.0.0.1
+    int rv = connect(fd, (const struct sockaddr *)&addr, sizeof(addr));
+    if (rv){
+        die("connect() failed");
+    }
+
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i){
+        cmd.push_back(argv[i]);
+    }
+    int32_t err = send_req(fd, cmd);
+    if (err){
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err){
+        goto L_DONE;
+    }
+
+L_DONE:
+    close(fd);
+    return 0;
+}
