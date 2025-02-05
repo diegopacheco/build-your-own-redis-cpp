0feb55fecee86d72e0c1ccbfbba794cca2ba79fc
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-04 22:23:46 -0800

    chapter 8/ progress

--- chapter-08/hashtable.h
+++ chapter-08/hashtable.h
@@ -3,16 +3,16 @@
 #include <stddef.h>
 #include <stdint.h>
 
-// hashtable node, should be embeded into the payload
-struct HNode{
+// hashtable node, should be embedded into the payload
+struct HNode {
     HNode *next = NULL;
     uint64_t hcode = 0;
 };
 
-// a simple fixed-size hashtable
+// a simple fixed-sized hashtable
 struct HTab {
     HNode **tab = NULL; // array of slots
-    size_t mask = 0;    // power of 2 array size, 2^n -1
+    size_t mask = 0;    // power of 2 array size, 2^n - 1
     size_t size = 0;    // number of keys
 };
 
@@ -22,10 +22,10 @@ struct HMap {
     HTab newer;
     HTab older;
     size_t migrate_pos = 0;
-}
+};
 
 HNode *hm_lookup(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *));
-void hm_insert(HMap *hmap,HNode *node);
-HNode *hm_delete(HMap *hmap,HNode *key, bool (*eq)(HNode *, HNode *));
-void hm_clear(HMap *hmap);
+void   hm_insert(HMap *hmap, HNode *node);
+HNode *hm_delete(HMap *hmap, HNode *key, bool (*eq)(HNode *, HNode *));
+void   hm_clear(HMap *hmap);
 size_t hm_size(HMap *hmap);
