f226c25053c69749f61998d723abd555cd6a4c38
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-08 21:08:33 -0800

    chapter-12/progress

--- chapter-12/build.sh
+++ chapter-12/build.sh
@@ -2,6 +2,5 @@
 
 mkdir -p target/
 
-g++ -Wall -Wextra -O2 -g common.h hashtable.cpp zset.cpp avl.cpp test_offset.cpp -o target/test_offset
 g++ -Wall -Wextra -O2 -g common.h hashtable.cpp zset.cpp avl.cpp server.cpp -o target/server
 g++ -Wall -Wextra -O2 -g common.h hashtable.cpp zset.cpp avl.cpp client.cpp -o target/client
