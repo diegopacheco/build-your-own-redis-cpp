c75da0217ffe37be1cf3cc1b355b063c02c18e80
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-08 21:04:57 -0800

    chapter-12/init

---
+++ chapter-12/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p target/
+
+g++ -Wall -Wextra -O2 -g common.h hashtable.cpp zset.cpp avl.cpp test_offset.cpp -o target/test_offset
+g++ -Wall -Wextra -O2 -g common.h hashtable.cpp zset.cpp avl.cpp server.cpp -o target/server
+g++ -Wall -Wextra -O2 -g common.h hashtable.cpp zset.cpp avl.cpp client.cpp -o target/client
