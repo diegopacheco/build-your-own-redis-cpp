7eeab85ac39085b8bfeaa74ecaea34429f38ea29
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-10 23:23:28 -0800

    chapter-14/init

---
+++ chapter-14/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p target/
+
+g++ -Wall -Wextra -O2 -g common.h hashtable.cpp heap.cpp zset.cpp avl.cpp thread_pool.cpp server.cpp -o target/server
+g++ -Wall -Wextra -O2 -g common.h hashtable.cpp heap.cpp zset.cpp avl.cpp client.cpp -o target/client
+g++ -Wall -Wextra -O2 -g heap.h test_heap.cpp -o target/test_heap
