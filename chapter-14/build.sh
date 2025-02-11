648cfa55bbd1070f587041bd9b05c4404cd8c287
diegopacheco <diego.pacheco.it@gmail.com>
2025-02-10 23:33:27 -0800

    chapter-14/progress

--- chapter-14/build.sh
+++ chapter-14/build.sh
@@ -4,4 +4,3 @@ mkdir -p target/
 
 g++ -Wall -Wextra -O2 -g common.h hashtable.cpp heap.cpp zset.cpp avl.cpp thread_pool.cpp server.cpp -o target/server
 g++ -Wall -Wextra -O2 -g common.h hashtable.cpp heap.cpp zset.cpp avl.cpp client.cpp -o target/client
-g++ -Wall -Wextra -O2 -g heap.h test_heap.cpp -o target/test_heap
